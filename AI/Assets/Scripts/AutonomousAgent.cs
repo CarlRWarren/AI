b9b23d010409e89202bc351ffb5954adc3779a2a
CarlRWarren <cwarren@student.neumont.edu>
2019-01-08 13:07:57 -0700

    Starting off

---
+++ AI/Assets/Scripts/AutonomousAgent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutonomousAgent : MonoBehaviour
+{
+    public Vector3 acceleration { get; set; } = Vector3.zero;
+    public Vector3 velocity { get; set; } = Vector3.zero;
+
+    void Start()
+    {
+     
+    }
+    
+    void Update()
+    {
+        acceleration = Vector3.zero;
+        Vector3 force = Vector3.forward;
+        ApplyForce(force);
+
+        velocity += acceleration * Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
+
+        Debug.Log(velocity);
+        Debug.DrawLine(transform.position, transform.position + velocity, Color.red);
+    }
+
+    void ApplyForce(Vector3 force)
+    {
+        acceleration = acceleration + force;
+    }
+}
